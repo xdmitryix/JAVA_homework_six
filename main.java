cee4dd0686d1513a366f3695816c27a07e6fb933
dmitry <rbdeman@yandex.ru>
2023-03-21 00:59:09 +0300

    сделал заготовку if

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 // Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
 // Создать множество ноутбуков.
@@ -40,5 +41,30 @@ public class main {
         noteBook noteBook7 = new noteBook(16, 500, "macos", "gray");
         ArrayList<noteBook> list = new ArrayList<>(Arrays.asList(noteBook1, noteBook2, noteBook3, noteBook4, noteBook5, noteBook6, noteBook7));
         System.out.println(list.toString());
+        System.out.println("Выберете параметр для поиска: 1-ОЗУ, 2-Жесткий диск, 3-Операционная система, 4-цвет.");
+        Scanner iScanner = new Scanner(System.in, "Cp866");
+        String command = iScanner.nextLine();
+        if (command.equals("ОЗУ")) {
+            
+        }else{
+            if (command.equals("1")) {
+
+            }else{
+                if (command.equals("2")) {
+            }else{
+                if (command.equals("3")){
+
+                }else{
+                    if (command.equals("4")){
+
+                }else{
+                    System.out.println("Некорректная команда!");
+                }
+            }
+        }
+
+
+    }
+}
     }
 }
