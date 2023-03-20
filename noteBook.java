b6b66d252f45c4d49c777d4d5a0794b97e67628a
dmitry <rbdeman@yandex.ru>
2023-03-21 00:17:24 +0300

    создал класс

--- noteBook.java
+++ noteBook.java
@@ -1,3 +1,22 @@
 public class noteBook {
     
+    private String ozu;
+    private int hardDisk;
+    private String OS;
+    private String color;
+
+    public noteBook(String ozu, int hardDisk, String OS, String color){
+        this.ozu = ozu;
+        this.hardDisk = hardDisk;
+        this.OS = OS;
+        this.color = color;
+    }
+
+
+
+
+    // 1 - ОЗУ
+// 2 - Объем ЖД
+// 3 - Операционная система
+// 4 - Цвет
 }
