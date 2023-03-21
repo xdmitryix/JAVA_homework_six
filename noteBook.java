4b3f0ef523495a31d4ccdf112daea7f2a6fe5340
dmitry <rbdeman@yandex.ru>
2023-03-21 18:45:37 +0300

    добавил getter

--- noteBook.java
+++ noteBook.java
@@ -12,6 +12,23 @@ public class noteBook {
         this.color = color;
     }
 
+
+    public int getOzu() {
+        return ozu;
+    }
+
+    public int getHardDisk() {
+        return hardDisk;
+    }
+
+    public String getOS() {
+        return OS;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
     @Override
     public String toString() {
         String res = String.format("OZU:%dgb HardDisk:%dgb OS:%s Color:%s", ozu, hardDisk, OS, color);
